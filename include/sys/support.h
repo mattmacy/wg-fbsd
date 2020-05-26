5d5544c7c70e788ee24948bdcb2ecf7cd611222e
Matt Macy <mmacy@FreeBSD.org>
2020-05-25 18:44:27 -0700

    define macros needed by platform optimized crypto code

--- include/sys/support.h
+++ include/sys/support.h
@@ -36,6 +36,43 @@ MALLOC_DECLARE(M_WG);
 
 #define ASSERT(x) MPASS(x)
 
+#define ___PASTE(a,b) a##b
+#define __PASTE(a,b) ___PASTE(a,b)
+#define __UNIQUE_ID(prefix) __PASTE(__PASTE(__UNIQUE_ID_, prefix), __COUNTER__)
+
+#define typeof(x) __typeof__(x)
+
+#define __typecheck(x, y) \
+		(!!(sizeof((typeof(x) *)1 == (typeof(y) *)1)))
+
+/*
+ * This returns a constant expression while determining if an argument is
+ * a constant expression, most importantly without evaluating the argument.
+ * Glory to Martin Uecker <Martin.Uecker@med.uni-goettingen.de>
+ */
+#define __is_constexpr(x) \
+	(sizeof(int) == sizeof(*(8 ? ((void *)((long)(x) * 0l)) : (int *)8)))
+
+#define __no_side_effects(x, y) \
+		(__is_constexpr(x) && __is_constexpr(y))
+
+#define __safe_cmp(x, y) \
+		(__typecheck(x, y) && __no_side_effects(x, y))
+
+#define __cmp(x, y, op)	((x) op (y) ? (x) : (y))
+
+#define __cmp_once(x, y, unique_x, unique_y, op) ({	\
+		typeof(x) unique_x = (x);		\
+		typeof(y) unique_y = (y);		\
+		__cmp(unique_x, unique_y, op); })
+
+#define __careful_cmp(x, y, op) \
+	__builtin_choose_expr(__safe_cmp(x, y), \
+		__cmp(x, y, op), \
+		__cmp_once(x, y, __UNIQUE_ID(__x), __UNIQUE_ID(__y), op))
+
+#define min_t(type, x, y)	__careful_cmp((type)(x), (type)(y), <)
+
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
@@ -44,6 +81,8 @@ typedef uint64_t  u64;
 typedef uint64_t  __le64;
 
 #define __must_check		__attribute__((__warn_unused_result__))
+#define asmlinkage
+#define __ro_after_init
 
 #define get_unaligned_le32(x) le32dec(x)
 #define get_unaligned_le64(x) le64dec(x)
